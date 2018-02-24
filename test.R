647625424d2cc16efbee932e8d34a93bef129ab5
Owen Ouyang <oouyang@J2PMR3212.wattsworth.com>
2018-02-24 08:51:49 -0500

    new cnn

--- test.R
+++ test.R
@@ -20,13 +20,30 @@ train[4,-1] %>% matrix(nrow=sqrt(dim(train)[2]-1),byrow=T) %>% apply(2,rev)%>% t
 train.label<-train[,1] %>% to_categorical()
 
 train.feature<-train[,-1] %>% normalize()
-test.feature<-test[,-1] %>% 
+test.feature<-test %>% normalize()
+
+dim(train.feature)<-c(nrow(train.feature),28,28,1)
+dim(test.feature)<-c(nrow(test.feature),28,28,1)
 
 model<-keras_model_sequential()
 
 model %>% 
+  layer_conv_2d(filters = 32, kernel_size = c(5,5),padding = 'Same',
+                activation = 'relu', input_shape = c(28,28,1))%>%
+  layer_conv_2d(filters = 32, kernel_size = c(5,5),padding = 'Same', 
+                activation ='relu')%>%
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_dropout(rate = 0.25) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3,3),padding = 'Same',
+                activation = 'relu')%>%
+  layer_conv_2d(filters = 64, kernel_size = c(3,3),padding = 'Same', 
+                activation ='relu')%>%
+  layer_max_pooling_2d(pool_size = c(2, 2), strides=c(2,2)) %>% 
+  layer_dropout(rate = 0.25) %>% 
+  layer_flatten() %>% 
   layer_dense(units=128,activation='relu',input_shape=c(784))%>%
   layer_dense(units=64,activation='relu')%>%
+  layer_dropout(rate = 0.5) %>%
   layer_dense(units=10,activation='softmax')
 
 model%>%compile(
@@ -38,8 +55,9 @@ model%>%compile(
 history<-model %>%  fit(
   train.feature,
   train.label,
-  epochs=10,
+  epochs=1,
   batch_size=5,
   validation_split=.1
 )
 
+plot(history)

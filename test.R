d453699a8c3e8420ea226d06add4501b09b5635a
Owen Ouyang <oouyang@J2PMR3212.wattsworth.com>
2018-02-24 00:37:36 -0500

    added test

---
+++ test.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(keras)
+library(tensorflow)
+library(tidyverse)
+
+setwd("C:/Users/oouyang/Dropbox/Analysis/Database/Digit Recognizer")
+
+train<-data.matrix(read_csv("train.csv"))
+test<-data.matrix(read_csv("test.csv"))
+
+dim(train)
+dim(test)
+
+colnames(train)
+
+train[,1] %>% table() %>% barplot(col=1:10)
+
+train[4,-1] %>% matrix(nrow=sqrt(dim(train)[2]-1),byrow=T) %>% apply(2,rev)%>% t() %>% image(col=grey.colors(255))
+
+train.label<-train[,1] %>% to_categorical()
+
+train.feature<-train[,-1] %>% normalize()
+test.feature<-test[,-1] %>% 
+
+model<-keras_model_sequential()
+
+model %>% 
+  layer_dense(units=128,activation='relu',input_shape=c(784))%>%
+  layer_dense(units=64,activation='relu')%>%
+  layer_dense(units=10,activation='softmax')
+
+model%>%compile(
+  loss='categorical_crossentropy',
+  optimizer='adam',
+  metrics='accuracy'
+)
+
+history<-model %>%  fit(
+  train.feature,
+  train.label,
+  epochs=10,
+  batch_size=5,
+  validation_split=.1
+)
+
